fcd00894c4dd5087fdcc57934404d30ad13e8c9a
ronie2020 <ronie.rodiana@gmail.com>
2025-11-27 02:57:36 +0700

    perbaikan logo

--- resources/views/library/kiosk.blade.php
+++ resources/views/library/kiosk.blade.php
@@ -15,8 +15,7 @@
         <!-- Header -->
         <div class="mb-8 animate-fade-in-down">
             <div class="inline-block p-4 rounded-full bg-gray-800/50 mb-4 border border-gray-700 shadow-lg">
-                {{-- Ikon Buku --}}
-                <svg class="w-16 h-16 text-emerald-400 mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                <x-application-logo class="w-20 h-20 mx-auto" />
             </div>
             <h1 class="text-4xl font-black text-white tracking-tight">BUKU TAMU PERPUSTAKAAN</h1>
             <p class="text-lg text-emerald-400 mt-2 font-medium uppercase tracking-widest">Silakan Scan Kartu Anggota</p>
