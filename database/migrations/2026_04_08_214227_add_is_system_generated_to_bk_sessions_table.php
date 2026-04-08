226122b6de7ac724db413b65a6db39c4607cbfb2
ronie2020 <ronie.rodiana@gmail.com>
2026-04-08 21:43:54 +0700

    migrate

---
+++ database/migrations/2026_04_08_214227_add_is_system_generated_to_bk_sessions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bk_sessions', function (Blueprint $table) {
+            // Menambahkan kolom boolean untuk menandai tiket otomatis
+            $table->boolean('is_system_generated')->default(false)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bk_sessions', function (Blueprint $table) {
+            $table->dropColumn('is_system_generated');
+        });
+    }
+};
